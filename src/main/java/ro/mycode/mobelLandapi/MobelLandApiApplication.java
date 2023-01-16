91afee27ebb7e80a8ded8ceb07d984c5a88a0245
Kati89 <kovacs.katalin@sapientia.ms.ro>
2023-01-16 05:32:38 -1000

    Primul commit

---
+++ src/main/java/ro/mycode/mobelLandapi/MobelLandApiApplication.java
@@ -0,0 +1,13 @@
+package ro.mycode.mobelLandapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobelLandApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobelLandApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/mycode/mobelLandapi/MobelLandApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.mycode.mobelLandapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobelLandApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
