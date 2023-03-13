cf2c194f4c74e8fe8ac1ce8d12ac83976bcd89bd
kati1989 <kati_1989@yahoo.com>
2023-03-13 06:56:57 -1000

    validators

--- src/main/java/ro/mycode/mobelLandapi/model/Mobila.java
+++ src/main/java/ro/mycode/mobelLandapi/model/Mobila.java
@@ -6,43 +6,59 @@ import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Size;
 
+//        text+=id+","+denumire+","+culoare+","+material+","+pret;
 @Data
-@Entity//creaza o table in baza de data
-@Table(name="mobila")
 @AllArgsConstructor
 @NoArgsConstructor
-public class Mobila implements  Comparable<Mobila> {
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    @Id
+@Entity(name="Mobila")//creaza o table in baza de data
+@Table(name="mobile")
 
-    private long id;
+public class Mobila implements  Comparable<Mobila> {
+
+    @Id
+    @SequenceGenerator(name="mobila_sequence",sequenceName = "mobila_sequence",allocationSize  =1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "mobila_sequence")
+
+    private Long id;
+
+    @Column(name="denumire",nullable=false)
+    @Size(min=4,message = "Trebuie sa fie min 4 caractere")
     private String denumire;
+
+    @Column(name="culoare",nullable = false)
+    @Size(max=10,message = "Cularea nu poate fi mai lung decat 10 caractere")
     private String culoare;
+
+    @Column(name="material",nullable = false)
+    @Size(min=2,message = "Materialul trebuie sa aiba min 2 caractere")
     private String material;
+
+    @Column
+    @Max(value=10000,message = "Pretul Nu poate fi mai mare decat 10000")
     private int pret;
 
 
     @Override
-    public String toString(){
-        String text="";
-        text+=id+","+denumire+","+culoare+","+material+","+pret;
-        return  text;
-    }
-    @Override
     public int compareTo(Mobila o) {
+     if(this.material.compareTo(o.material)>0){
+         return 1;
+     }
+
+     if(this.material.compareTo(o.material)<0){
+         return  -1;
+     }else
+
+
         return 0;
     }
 
-    public int compare(Object o){
-        Mobila mobila=(Mobila) o;
-        if(this.pret>mobila.pret){
-            return 1;
-        }else if(this.pret< mobila.pret){
-            return 0;
-        }
-
-        return -1;
+    @Override
+    public  boolean equals(Object o){
+        Mobila mobila=(Mobila)o;
+        return  this.material.equals(mobila.material);
     }
 }
 
