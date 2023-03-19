eb7d9af96f164f5aa458e5cfce6329f4fc9bbf53
kati1989 <kati_1989@yahoo.com>
2023-03-19 03:36:00 -1000

    constatns

---
+++ src/main/java/ro/mycode/mobelLandapi/constants/Constants.java
@@ -0,0 +1,11 @@
+package ro.mycode.mobelLandapi.constants;
+
+public class Constants {
+
+    public  static  final String EMPTY_DATABASE_EXCEPTION="List of furniture is empty";
+
+    public  static  final String FURNITURE_NOT_FOUND_EXCEPTION="Furniture not found";
+
+    public  static final String FURNITURE_DUPLICATION="Existing furniture";
+
+}
